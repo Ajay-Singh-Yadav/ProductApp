34ed8539c1444f704588e5c09c6151521de6566d
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-09-22 12:41:49 +0530

    initnitial commit

--- src/redux/AuthContext.jsx
+++ src/redux/AuthContext.jsx
@@ -1,46 +1,14 @@
-import React, {createContext, useState, useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-export const AuthContext = createContext();
-
-export const AuthProvider = ({children}) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [coupons, setCoupons] = useState([]);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      const simulatedLoggedIn = Math.random() > 0.1;
-      setIsLoggedIn(simulatedLoggedIn);
-
-      if (simulatedLoggedIn) {
-        setUser({name: 'Hardik', id: '123'});
-        setCoupons([
-          {id: 'c1', code: 'SAVE10', discount: '10% off'},
-          {id: 'c2', code: 'FREESHIP', discount: 'Free Shipping'},
-        ]);
-      } else {
-        setUser(null);
-        setCoupons([]);
-      }
-    };
-    checkLoginStatus();
-  }, []);
-
-  const login = (userData, userCoupons) => {
-    setIsLoggedIn(true);
-    setUser(userData);
-    setCoupons(userCoupons);
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-    setUser(null);
-    setCoupons([]);
-  };
-
+const AuthContext = () => {
   return (
-    <AuthContext.Provider value={{isLoggedIn, coupons, user, login, logout}}>
-      {children}
-    </AuthContext.Provider>
+    <View>
+      <Text>AuthContext</Text>
+    </View>
   );
 };
+
+export default AuthContext;
+
+const styles = StyleSheet.create({});
